2dd4f8750d2c375ad4e40c041aa6f58ce98cf6f7
puran chand <pchand757@gmail.com>
2019-02-05 08:29:04 +0530

    add css

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,21 +1,35 @@
 <template>
-  <div class="hello">
+    <div class="container mx-auto hello">
     <h1>Todo List Created in Vue.js</h1>
-    <div>
-      <input v-model="message" placeholder="Edit Me"/>
-      <button type='button' v-on:click="addText()">Add Click</button>
-     <ul>
-       <li v-for="(todo,index) in todos" >
-        {{index +1}} {{todo}} 
-         <button type='button'  v-on:click="deleteText(todo)">x</button>
-       </li>
-     </ul>
+    <div class="mx-auto">
+      <!-- <input  placeholder="Edit Me"/>
+      <button class="btn btn-primary" type='button' >Add Click</button> -->
+      <div class="input-group mb-3 addInput">
+        <input type="text" class="form-control" v-model="message" placeholder="Add text" aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2" v-on:click="addText()">@Add</span>
+        </div>
+      </div>
+      <div class="card align-center" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li  class="list-group-item" v-for="(todo,index) in todos" :key="index" >
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary" > {{index+1}} </button>
+                <button type="button" class="btn btn-secondary centerBtn"> {{todo}} </button>
+                <button class="btn btn-secondary" type='button'  v-on:click="deleteText(todo)">x</button>
+              </div>
+          
+            
+          </li>
+        </ul>
+      </div>
     </div>
   
   </div>
 </template>
 
 <script>
+
 
 export default {
   
@@ -42,6 +56,8 @@ export default {
   },
 
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -59,5 +75,15 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.centerBtn{
+  min-width: 150px;
+}
+
+
+
+.addInput {
+    width: 400px;
 }
 </style>
